c1050aad8afb327817a1fc369afc417f0deeeb9b
Niklas Zeeb <sesoui72@googlemail.com>
2019-12-22 02:28:02 +0100

    Initial Tech-demo barebones, will get more once I did more

---
+++ Assets/Scripts/MainMenu/PizzaScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PizzaScript : MonoBehaviour
+{
+    // Necessary Things. (Private fields)
+    private string _myText;
+    private int _argProgress;
+    
+    // Necessary Things. (Unity Fields)
+    [Tooltip("Title Text Box here")] public Text TitleTextBox;
+    [Tooltip("Speaker Text Box here")] public Text SpeakTextBox;
+    [Tooltip("Secret Codes here!")] public string[] SecretCodes;
+    [Tooltip("Input Field here!")] public InputField SecretInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myText = "";
+        _argProgress = 0;
+        SecretInput.interactable = true;
+        SpeakTextBox.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SecretDetected();
+    }
+
+    // Update this and SecretCodes + Materials whenever you add something new!
+    private void SecretDetected()
+    {
+        _myText = SecretInput.text;
+
+        if (SecretCodes.Contains(_myText))
+        {
+            if (_myText == "takemetothepizza")
+                TitleTextBox.color = Color.yellow;
+
+            if (_myText == "hello?" && _argProgress == 0)
+            {
+                SpeakTextBox.text = "Ah yes, Hello there! 't was getting quite lonely in here...";
+                _argProgress = 1;
+                SecretInput.text = "";
+            }
+
+            if (_myText == "what is this?" && _argProgress == 1)
+            {
+                SpeakTextBox.text =
+                    "Well, it can be what you think it would be. I define it as some sort of black void though.";
+                _argProgress = 2;
+                SecretInput.text = "";
+            }
+
+            if (_myText == "uh-huh" && _argProgress == 2)
+            {
+                SpeakTextBox.text =
+                    "You seem not surprised, eh? And what if I tell you that someone is standing right behind you?";
+                _argProgress = 3;
+                SecretInput.text = "";
+            }
+
+            if (_myText == "well ok then." && _argProgress == 3)
+            {
+                SpeakTextBox.text = "...Fine, you're one party pooper.";
+                _argProgress = 4;
+                SecretInput.text = "";
+            }
+
+            if (_myText == "..." && _argProgress == 4)
+            {
+                SpeakTextBox.text = "...ugh, ok, you know what? Fine. I'll go. See you never...";
+                SecretInput.text = "fucktard.";
+                _argProgress = 0;
+            }
+        }
+        else
+            TitleTextBox.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/MainMenu/RandomFactScript.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+
+public class RandomFactScript : MonoBehaviour
+{
+    // Necessary things. (Private Fields
+    private Timer _randFactTimer;
+    private string[] _randFacts = new string[]
+    {
+        "This game was inspired by VVVVVV.",
+        "Bananas always grow towards the sun.",
+        "You are wasting your time reading these facts",
+        "Typing in 'takemetothepizza' won't trigger anything...",
+        "Why jump when you can flip the gravity?",
+        "Powered by Portals since... idk when.",
+        "Developer ran out of facts to display. Enjoy this waste of space."
+    };
+    private Random _rand;
+    private string _fact;
+    
+    // Necessary Things. (Unity Fields)
+    [Tooltip("Random Fact Text Box here")]
+    public Text RandomFactText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomFactText = GetComponent<Text>();
+        _rand = new Random();
+        _randFactTimer = new Timer();
+        SetupTimer();
+        _fact = "Welcome to Infinicube!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RandomFactText.text = _fact; // Write fact into the TextBox
+    }
+    
+    // Custom methods
+    private void SetupTimer()
+    {
+        var startTime = DateTime.Now;
+        _randFactTimer.Interval = 15000; // 15 seconds
+        _randFactTimer.Elapsed += RandFactTimerOnElapsed;
+        _randFactTimer.AutoReset = true;
+        _randFactTimer.Enabled = true;
+        Debug.Log($"[Infinicube.MainMenu.RandFactTimer] Timer has been started at {startTime}");
+    }
+
+    private void RandFactTimerOnElapsed(object sender, ElapsedEventArgs e)
+    {
+        var changeTime = e.SignalTime;
+        var randIdx = _rand.Next(_randFacts.Length); // Get next Status to display
+        var fact = _randFacts[randIdx]; // Assign chosen fact.
+        _fact = fact; // More Assignments
+        Debug.Log($"[Infinicube.MainMenu.RandFactTimer] Fact changed at {changeTime}\n" +
+                   $"[Infinicube.MainMenu.RandFactTimer] Current fact: {fact}");
+    }
+}

b63c6ea822e78329b0442fe38995f83fc472b6fb
Niklas Zeeb <sesoui72@googlemail.com>
2020-01-04 22:11:57 +0100

    Mehr stuff argh

---
+++ Assets/Scripts/SceneChangerScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChangerScript : MonoBehaviour
+{
+    public Button Button;
+    public Button Exit;
+    public string Scene;
+
+    private void Start()
+    {
+        Button.onClick.AddListener(ChangeScene);
+        Exit.onClick.AddListener(ExitGame);
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadSceneAsync(Scene);
+    }
+    
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+}

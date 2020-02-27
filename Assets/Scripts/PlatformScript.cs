3ca0505450ad7da2c4acf3bea09489b1a7ecc63e
BlueBrainZero <59562897+BlueBrainZero@users.noreply.github.com>
2020-02-27 19:35:47 +0100

    Warum

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform p1;
+    [SerializeField]
+    Transform p2;
+    [SerializeField]
+    public float speed;
+    Vector3 nextP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextP = p2.position;
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        if (transform.position == p2.position)
+            nextP = p1.position;
+        if (transform.position == p1.position)
+            nextP = p2.position;
+        transform.position = Vector3.MoveTowards(transform.position, nextP, speed * Time.deltaTime);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(p1.position, p2.position);
+    }
+}
